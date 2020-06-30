3f54b706871b2e80e2f9a350fbb8214d351047bf
Mdmehedihasanrifat <mehedihasan199607@gmail.com>
2020-07-01 04:30:32 +0600

    compelete fucntion

--- js/main.js
+++ js/main.js
@@ -1,7 +1,7 @@
 //li underline
 
 
-$("li").click(function () {
+$("ul").on("click","li",(function () {
   $(this).toggleClass('complete');
 
 //
@@ -21,14 +21,24 @@ $("li").click(function () {
 // }
 
 
-});
+}));
 
 
 // span event add
-$("span").click(function (event) {
+$("ul").on("click","span", (function (event) {
 $(this).parent().fadeOut(500,function () {
   $(this).remove();
 });
   event.stopPropagation();
 
+}));
+ // input add
+
+$("input[type='text']").keypress(function (event) {
+if(event.which===13){
+
+  let todo=$(this).val();
+  $(this).val("");
+$("ul").append("<li><span>X</span> "+todo+"</li>");
+}
 })
