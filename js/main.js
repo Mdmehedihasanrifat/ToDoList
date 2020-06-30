8e1fc242c36af092fe3e61e069bd645d8dfbd9df
Mdmehedihasanrifat <mehedihasan199607@gmail.com>
2020-07-01 03:33:19 +0600

    complete line through

---
+++ js/main.js
@@ -0,0 +1,24 @@
+//li underline
+
+
+$("li").click(function () {
+  $(this).toggleClass('complete');
+
+//
+// if($(this).css("color") ==="rgb(128, 128, 128)"){
+//
+//   $(this).css({
+//     textDecoration:"none",
+//     color: "black"
+//   });
+//
+// }
+// else {
+//   $(this).css({
+//     textDecoration: "line-through",
+//     color: "grey"
+//   });
+// }
+
+
+});
